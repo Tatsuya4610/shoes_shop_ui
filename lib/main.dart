867c3232d4d7bbc0f4068607052acdb81795789c
石川達也 <tatsuya7610@icloud.com>
2021-01-29 11:53:08 +0900

    3th commit

--- lib/main.dart
+++ lib/main.dart
@@ -4,36 +4,28 @@ import 'package:shoes_shop_ui_flutter/model/shoes_model.dart';
 import 'package:shoes_shop_ui_flutter/screen/desc_screen.dart';
 import 'screen/shoes_screen.dart';
 
-//YouTubeチャンネル　配信者　https://www.youtube.com/c/Utter_Flutter/featured
-//動画リンク。https://www.youtube.com/watch?v=M1HOKEOODFG3
-
-
-void main() => runApp(
-      MultiProvider(
-        providers: [
-          ChangeNotifierProvider(
-            create: (_) => ShoesModel(),
-          ),
-        ],
-        child: MyApp(),
-      ),
-    );
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'ShoesShopUI',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return ChangeNotifierProvider(
+      create: (_) => ShoesModel(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'ShoesShopUI',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: Scaffold(
+          body: ShoesScreen(),
+        ),
+        routes: {
+          DescScreen.id: (context) => DescScreen(),
+        },
       ),
-      home: Scaffold(
-        body: ShoesScreen(),
-      ),
-      routes: {
-        DescScreen.id: (context) => DescScreen(),
-      },
     );
   }
 }
--- lib/widgets/choice_color.dart
+++ lib/widgets/choice_color.dart
@@ -8,48 +8,53 @@ class ChoiceColor extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.symmetric(horizontal: 10),
-      child: Row(
-        children: <Widget>[
-          Expanded(
-            child: Stack(
+        padding: EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('ボタンで画像の色変更可能。',style: TextStyle(fontSize: 12),),
+            Row(
               children: <Widget>[
-                Positioned(
-                    left: 90,
-                    child: _ButtonColor(
-                      color: Color(0xffC6D642),
-                      index: 4,
-                      imageUrl: 'assets/image/verde.png',
-                    )),
-                Positioned(
-                    left: 60,
-                    child: _ButtonColor(
-                      color: Color(0xffFFAD29),
-                      index: 3,
-                      imageUrl: 'assets/image/amarillo.png',
-                    )),
-                Positioned(
-                    left: 30,
-                    child: _ButtonColor(
-                      color: Color(0xff2099F1),
-                      index: 2,
-                      imageUrl: 'assets/image/azul.png',
-                    )),
-                _ButtonColor(
-                  color: Color(0xff364D56),
-                  index: 1,
-                  imageUrl: 'assets/image/negro.png',
+                Expanded(
+                  child: Stack(
+                    children: <Widget>[
+                      Positioned(
+                          left: 90,
+                          child: _ButtonColor(
+                            color: Color(0xffC6D642),
+                            index: 4,
+                            imageUrl: 'assets/image/verde.png',
+                          )),
+                      Positioned(
+                          left: 60,
+                          child: _ButtonColor(
+                            color: Color(0xffFFAD29),
+                            index: 3,
+                            imageUrl: 'assets/image/amarillo.png',
+                          )),
+                      Positioned(
+                          left: 30,
+                          child: _ButtonColor(
+                            color: Color(0xff2099F1),
+                            index: 2,
+                            imageUrl: 'assets/image/azul.png',
+                          )),
+                      _ButtonColor(
+                        color: Color(0xff364D56),
+                        index: 1,
+                        imageUrl: 'assets/image/negro.png',
+                      ),
+                    ],
+                  ),
+                ),
+                AddButton(
+                  text: '色の選択確定',
+                  color: Color(0xffFFC675),
                 ),
               ],
             ),
-          ),
-          AddButton(
-            text: '色の選択確定',
-            color: Color(0xffFFC675),
-          ),
-        ],
-      ),
-    );
+          ],
+        ));
   }
 }
 
